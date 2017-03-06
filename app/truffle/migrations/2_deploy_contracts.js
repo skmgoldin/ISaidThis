35c460b3bfcc0dc6fd4e9e2600b2d5e424872112
Mike Goldin <mike.goldin@consensys.net>
2017-03-06 12:07:37 -0500

    Customize deploy for easy click testing

--- app/truffle/migrations/2_deploy_contracts.js
+++ app/truffle/migrations/2_deploy_contracts.js
@@ -1,5 +1,29 @@
-var ISaidThis = artifacts.require("./ISaidThis.sol");
+var ISaidThis = artifacts.require("./ISaidThis.sol")
 
-module.exports = function(deployer) {
-  deployer.deploy(ISaidThis);
-};
+const raw = 'I love cats!'
+
+module.exports = 
+function(deployer) {
+  deployer.then(function() {
+    return deployer.deploy(ISaidThis)
+    .then(function(instance) {
+      return ISaidThis.deployed()
+    })
+    .then(function(instance) {
+      return instance.addProofByRaw(raw)
+    })
+    .then(function() {
+      return ISaidThis.deployed()
+    })
+    .then(function(instance) {
+      return instance.verifyRaw(raw)
+    })
+    .then(function(block) {
+      console.log(block)
+    })
+  })
+}
+
+
+
+
